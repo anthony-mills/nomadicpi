0cae7134a57de33c52de04078e9fde08578e847e
Anthony Mills <anthony.mills@tmsw.com>
2020-08-30 22:24:48 +1200

    Added the ability to pause and play mpd

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import sys
 import logging
 import threading
 import lib.gps as gps
+import lib.mpd as mpd
 
 from PyQt5 import QtWidgets
 
@@ -15,18 +16,59 @@ class mainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mainWindow, self).__init__()
 
+        # Connect to the MPD daemon
+        self.mpd = mpd.MpdLib()
+        self.mpd.connect_mpd()
+        
+        # Setup the UI
         self.ui = Ui_NomadicPI()
         self.ui.setupUi(self)
-        self.ui.QuitButton.clicked.connect(self.exit_application)
         
+        self.ui.QuitButton.clicked.connect(self.exit_application)  
+        self.ui.MusicPlay.clicked.connect(self.mpd.play_playback)      
+        self.ui.RandomPlayback.clicked.connect(self.mpd.random_playback)
+        self.ui.ConsumptionPlayback.clicked.connect(self.mpd.consumption_playback)
+        self.ui.UpdateDatabase.clicked.connect(self.mpd.update_library)        
+                
         self.update_content()
     
     def update_content(self):
+        """
+        Create a timer and periodically update the UI information
+        """              
         self.update_gps()
+        self.update_mpd()
         
-        threading.Timer(1, self.update_content).start()        
+        threading.Timer(0.3, self.update_content).start()  
+        
+    def update_mpd(self):
+        mpdStatus = self.mpd.get_status()
+
+        if mpdStatus.get('updating_db') is None:
+            self.ui.UpdateDatabase.setText( "Update Database" )
+        else:
+            self.ui.UpdateDatabase.setText( "Database Updating..." )   
+
+        if mpdStatus['state'] == 'play':
+            self.ui.MusicPlay.setText( "Pause" )
+        else:
+            self.ui.MusicPlay.setText( "Play" )     
+                        
+        if int(mpdStatus['random']) == 0:
+            self.ui.RandomPlayback.setText( "Enable Random Playback" )
+        else:
+            self.ui.RandomPlayback.setText( "Disable Random Playback" )            
+
+        if int(mpdStatus['consume']) == 0:
+            self.ui.ConsumptionPlayback.setText( "Enable Consumption Playback" )
+        else:
+            self.ui.ConsumptionPlayback.setText( "Disable Consumption Playback" )            
+        
         
     def update_gps(self):
+        """
+        Get the current GPS information and update the UI
+        """                
         gps_info = None
         
         if gps.gpsd_socket is None:
@@ -42,8 +84,8 @@ class mainWindow(QtWidgets.QMainWindow):
         
         if gps_info is not None:    
             try:
-                # Get the current GPS speed
-                cur_speed = gps_info.speed()
+                # Get the current GPS speed and convert from m/s to km/h
+                cur_speed = int(3.6 * gps_info.speed() )
                 self.ui.CurrentSpeed.setText( str(cur_speed) )            
                 
                 # Get the current Altitude
@@ -65,6 +107,7 @@ class mainWindow(QtWidgets.QMainWindow):
                 print(e)
         
     def exit_application():
+        self.mpd.close_mpd()
         sys.exit()
 
 if __name__ == '__main__':        
---
+++ src/lib/mpd.py
@@ -0,0 +1,85 @@
+from mpd import MPDClient
+
+class MpdLib():
+    def __init__(self):
+        self.mpd_host = '127.0.0.1'
+        self.mpd_port = 6600       
+        
+    def set_mpd_host(self, mpd_host):
+        """
+        Set the IP or hostname the MPD daemon is running on
+        """        
+        self.mpd_host = mpd.host
+        
+    def set_mpd_port(self, mpd_port):
+        """
+        Set the port the MPD daemon is running on
+        """         
+        self.mpd_port = mpd_port
+        
+    def connect_mpd(self):
+        """
+        Attempt to connect to the MPD daemon
+        """         
+        self.client = MPDClient()
+        self.client.timeout = 10
+
+        try:
+            self.client.connect(self.mpd_host, self.mpd_port) 
+        except Exception as e:
+            print("MPD Error: " + str(e))
+
+    def get_status(self):
+        """
+        Return the status of the mpd daemon
+        """         
+        return self.client.status()   
+        
+    def play_playback(self):
+        """
+        Change the play state
+        """    
+        status = self.client.status() 
+        
+        if status.get('state') == 'play':
+            self.client.pause()         
+        else:
+            self.client.play()          
+
+    def update_library(self):
+        """
+        Update the MPD library
+        """         
+        status = self.client.status() 
+        
+        if status.get('updating_db') is None:
+            self.client.update()   
+        
+    def random_playback(self):
+        """
+        Change the random playback state
+        """         
+        status = self.client.status()  
+        
+        if int(status['random']) == 0:
+            self.client.random(1)
+        else:
+            self.client.random(0)            
+        
+    def consumption_playback(self):
+        """
+        Change the consume playback state
+        """         
+        status = self.client.status()  
+        
+        if int(status['consume']) == 0:
+            self.client.consume(1)
+        else:
+            self.client.consume(0)    
+                                            
+    def close_mpd(self):
+        """
+        Close the connection to the MPD daemon
+        """         
+        self.client.close()
+        self.client.disconnect()        
